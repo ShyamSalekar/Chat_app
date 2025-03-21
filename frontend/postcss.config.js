58608d74a19e7d1ea28e45a3baf7539847bde44f
Shyam Jayram Salekar <shyamsalekar25@gmail.com>
2025-03-21 20:46:07 +0530

    Normalize line endings

---
+++ frontend/postcss.config.js
@@ -0,0 +1,14 @@
+export default {
+  plugins: {
+    // '@tailwindcss/postcss': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+// module.exports = {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// };
